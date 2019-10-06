53cdfec1bf3f9ecd41b2ead0472f49b25842ba3b
Mau <mauricio.marin.201@gmail.com>
2019-10-05 18:31:33 -0600

    Class Imagen returns grids with the contour of the image

--- Probabilistic/Imagen.py
+++ Probabilistic/Imagen.py
@@ -9,37 +9,14 @@ class Imagen:
             self.image = Image.open(filename)
             self.image = self.image.convert('RGB')
             self.width, self.height = self.image.size
+
             self.coordinates = []
+            self.edges = []
 
             self.iterate_image()
 
         except FileNotFoundError:
             print("Invalid value")
-
-    def is_background(self, percentage, x1, x2, y1, y2, box):
-
-        total_r = 0
-        total_g = 0
-        total_b = 0
-
-        for number in range(percentage):
-            pixel1 = random.randint(x1, x2 - 1)
-            pixel2 = random.randint(y1, y2 - 1)
-            red, green, blue = self.image.getpixel((pixel1, pixel2))
-            total_r += red
-            total_g += green
-            total_b += blue
-
-        avg_red = int(round(total_r / percentage))
-        avg_green = int(round(total_g / percentage))
-        avg_blue = int(round(total_b / percentage))
-
-        if avg_red != 0 and avg_green != 0 and avg_blue != 0:
-            color = (avg_red, avg_green, avg_blue)
-            cuadrante = Grid(color, box)
-            self.coordinates.append(cuadrante)
-            print("Average of", box, "=", avg_red, avg_green, avg_blue)
-
 
     def iterate_image(self):
         x_squares = self.width // 16
@@ -68,8 +45,47 @@ class Imagen:
                 count_y += 1
             count_x += 1
 
+    def is_background(self, percentage, x1, x2, y1, y2, box):
+        total_r = 0
+        total_g = 0
+        total_b = 0
+
+        for number in range(percentage):
+            pixel1 = random.randint(x1, x2 - 1)
+            pixel2 = random.randint(y1, y2 - 1)
+            red, green, blue = self.image.getpixel((pixel1, pixel2))
+            total_r += red
+            total_g += green
+            total_b += blue
+
+        avg_red = int(round(total_r / percentage))
+        avg_green = int(round(total_g / percentage))
+        avg_blue = int(round(total_b / percentage))
+
+        if avg_red != 0 and avg_green != 0 and avg_blue != 0:
+            color = (avg_red, avg_green, avg_blue)
+            cuadrante = Grid(color, box)
+
+            if avg_red <= 30 and avg_green <= 30 and avg_blue <= 30:
+                self.edges.append(cuadrante)
+            else:
+                self.coordinates.append(cuadrante)
+
+            # print("Average of", box, "=", avg_red, avg_green, avg_blue)
+
+    def get_edges(self):
+        return self.edges
+
+    def get_coordinates(self):
+        return self.coordinates
 
 
+im = Imagen("bird.png")
 
-im = Imagen("cyndaquill.png")
-print("Meow")
+coordinates = im.get_coordinates()
+for i in coordinates:
+    print("Average of", i.get_coordinate(), "is", i.get_color())
+
+edges = im.get_edges()
+for i in edges:
+    print("Average of", i.get_coordinate(), "is", i.get_color())
